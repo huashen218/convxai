281b316d191a77b29984bfd4dfc554fdbd8a0c1f
huashen218 <huashen218@gmail.com>
2022-11-03 21:33:14 -0400

    update

--- convxai/runners/main_client.sh
+++ convxai/runners/main_client.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -x;
 set -e;
-export PYTHONPATH=/home/huashen218/workspace/convxai/
-RUN_SERVICE_DIR="/home/huashen218/workspace/convxai/convxai/services/web_service";
-python $RUN_SERVICE_DIR/web_server.py
+# export PYTHONPATH=/home/huashen218/workspace/convxai/
+# RUN_SERVICE_DIR="/home/huashen218/workspace/convxai/convxai/services/web_service";
+
+export PYTHONPATH=/home/hqs5468/hua/workspace/projects/convxai/
+RUN_SERVICE_DIR="/home/hqs5468/hua/workspace/projects/convxai/convxai";
+
+python $RUN_SERVICE_DIR//services/web_service/web_server.py
