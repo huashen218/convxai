33f26bcbc60d9132f2b8741af585cc2671dfd610
huashen218 <huashen218@gmail.com>
2023-01-12 00:10:41 -0500

    data_migration

--- convxai/runners/main_server.sh
+++ convxai/runners/main_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x;
 set -e;
-export PYTHONPATH=/home/hqs5468/hua/workspace/projects/convxai/
-RUN_SERVICE_DIR="/home/hqs5468/hua/workspace/projects/convxai/convxai";
+export PYTHONPATH=/data/data/hua/workspace/projects/convxai/
+RUN_SERVICE_DIR="/data/data/hua/workspace/projects/convxai/convxai";
 
 CUDA_VISIBLE_DEVICES=2 python $RUN_SERVICE_DIR/services/run_server/run.py \
                         --config-path $RUN_SERVICE_DIR/configs/service_config.yml \
