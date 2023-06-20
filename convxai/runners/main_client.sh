0a58faac55d28a4d87e34708c678a0a5cc153a42
huashen218 <huashen218@gmail.com>
2023-06-20 00:43:22 -0400

    update

--- convxai/runners/main_client.sh
+++ convxai/runners/main_client.sh
@@ -7,4 +7,4 @@ RUN_SERVICE_DIR="/home/huashen218/workspace/convxai/convxai";
 # export PYTHONPATH=/home/hqs5468/hua/workspace/projects/convxai/
 # RUN_SERVICE_DIR="/home/hqs5468/hua/workspace/projects/convxai/convxai";
 
-python $RUN_SERVICE_DIR//services/web_service/web_server.py
+python $RUN_SERVICE_DIR/services/web_service/web_server.py
